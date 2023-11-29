fb9f436b16a226564436ae806135697758cf3e76
pedro-aldana <pedroaldana987@gmail.com>
2023-11-29 10:09:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + Vite + Tailwind css
+
+
+
---
+++ src/components/Cliente.jsx
@@ -0,0 +1,44 @@
+import { useNavigate, Form, redirect } from "react-router-dom"
+import { eliminarCliente } from "../data/clientes"
+
+export async function action({params}) {
+
+  await eliminarCliente(params.clienteId)
+  
+  return redirect('/')
+}
+
+function Cliente({cliente}) {
+  const navigate = useNavigate()
+  const {nombre, empresa, email, telefono, id} = cliente
+
+  return (
+    <tr className="border-b">
+      <td className="p-6 space-y-2">
+            <p className="text-2xl text-gray-600">{nombre}</p>
+            <p>{empresa}</p>
+      </td>
+
+      <td className="p-6">
+            <p className="text-gray-600"><span className="text-gray uppercase font-bold">Email:</span>{email}</p>
+            <p className="text-gray-600"><span className="text-gray uppercase font-bold">Tel:</span>{telefono}</p>
+      </td>
+
+      <td className="p-6 flex gap-3">
+        <button type="button" className="text-blue-600 hover:text-blue-700 font-bold text-sm" onClick={() => navigate(`/clientes/${id}/editar`)}>Editar</button>
+
+        <Form method="post" action={`/clientes/${id}/eliminar`} onSubmit={(e) => {
+          if(!confirm('¿Deseas eliminar este registro?')){
+            e.preventDefault()
+          }
+        }}>
+          <button type="submit" className="text-red-600 hover:text-red-700 font-bold text-sm">Eliminar</button>
+        </Form>
+
+        
+      </td>
+    </tr>
+  )
+}
+
+export default Cliente
